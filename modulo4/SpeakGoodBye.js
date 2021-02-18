1b80ae00ca8901a735443a59de26a106ff407d87
josepm2001 <78082988+josepm2001@users.noreply.github.com>
2021-02-18 15:56:25 +0100

    Add files via upload

---
+++ modulo4/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (nombre) {
+    console.log(speakWord + " " + nombre);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ modulo4/SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (nombre) {
+    console.log(speakWord + " " + nombre);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(window);
